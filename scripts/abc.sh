d3d12a7b6a6cab816534b3fac687f4635e2e3bd4
Baruch Sterin <baruchs@gmail.com>
2015-11-05 01:37:50 -0800

    Update abc.sh script to take advantage of the new behaviour of -q to get rid of explicit abc.rc

---
+++ scripts/abc.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Setup the ABC/Py environment and run the ABC/Py executable
+# (ABC/Py stands for ABC with embedded Python)
+#
+# ABC/Py expects the following directory structure
+#
+# abc_root/
+#   bin/
+#     abc - this script
+#     pyabc-exe - the ABC executable
+#	  *.sh - engine scripts
+#   lib/
+#     pyabc/pyzz/pyliveness/pyaig/super_prove/etc - python libraries
+#     python_library.zip - The Python standard library. Only if not using the system Python interpreter.
+#     *.so - Python extensions, Only if not using the system Python interpreter.
+#   scripts/
+#     *.py - default directory for python scripts
+#             
+
+abspath()
+{
+    local cwd="$(pwd)"
+    cd "$1"
+    echo "$(pwd)"
+    cd "${cwd}"
+}
+
+self=$0
+
+self_dir=$(dirname "${self}")
+self_dir=$(abspath "${self_dir}")
+
+abc_root=$(dirname "${self_dir}")
+abc_exe="${abc_root}/bin/pyabc.exe"
+
+export PATH="${abc_root}/bin:$PATH"
+
+export PYTHONPATH="${abc_root}/lib":"${PYTHONPATH}"
+
+if [ -f "${abc_root}/lib/python_library.zip" ] ; then
+    export PYTHONHOME="${abc_root}"
+    export PYTHONPATH="${abc_root}/lib/python_library.zip":"${PYTHONPATH}"
+fi
+
+if [ "$1" = "--debug" ]; then
+    shift
+    abc_debugger="$1"
+    shift
+    
+    echo export PYTHONHOME=$PYTHONHOME
+    echo export PYTHONPATH=$PYTHONPATH
+
+    echo export ABC_PYTHON_SCRIPTS=$ABC_PYTHON_SCRIPTS
+    echo export ABC_PYTHON_ABC_RC=$ABC_PYTHON_ABC_RC
+fi
+
+if [ -f "${abc_root}/lib/super_prove/abc.rc" ] ; then
+    exec ${abc_debugger} "${abc_exe}" -s -q "source -s ${abc_root}/lib/super_prove/abc.rc" "$@"
+else
+    exec ${abc_debugger} "${abc_exe}" -s "$@"
+fi
